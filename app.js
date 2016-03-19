002c95013fcd92ff91f64f8288f6e2c8d92235f0
pasuvan <muthuvel.ns@gmail.com>
2016-03-19 17:38:27 +0530

    first

---
+++ app.js
@@ -0,0 +1,8 @@
+var app=angular.module('two_way',[]);
+
+app.config(['$httpProvider', function($httpProvider) {
+        $httpProvider.defaults.useXDomain = true;
+        $httpProvider.defaults.headers.common = 'Content-Type: application/json';
+        delete $httpProvider.defaults.headers.common['X-Requested-With'];
+    }
+]);
